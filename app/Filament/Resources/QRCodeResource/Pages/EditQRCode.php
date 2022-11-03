147f9221536ab8cd268edd7132629836752eb6b2
Daniël van der Spoel <contact@danielvdspoel.nl>
2022-11-02 22:06:11 -0400

    Add download functionality

--- app/Filament/Resources/QRCodeResource/Pages/EditQRCode.php
+++ app/Filament/Resources/QRCodeResource/Pages/EditQRCode.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\QRCodeResource\Pages;
 
 use App\Filament\Resources\QRCodeResource;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Pages\Actions\Action;
@@ -19,7 +21,17 @@ class EditQRCode extends EditRecord
         return [
             Actions\DeleteAction::make(),
             Action::make('preview')
-                ->action(fn () => $this->record->advance())
+                ->action(function() {
+                    QrCode::size($this->record->size)
+                        ->color($this->record->getColorRGB()['r'], $this->record->getColorRGB()['g'], $this->record->getColorRGB()['b'])
+                        ->backgroundColor($this->record->getBackgroundColorRGB()['r'], $this->record->getBackgroundColorRGB()['g'], $this->record->getBackgroundColorRGB()['b'])
+                        ->style($this->record->style)
+                        ->eye($this->record->eye_style)
+                        ->errorCorrection($this->record->error_correction_level)
+                        ->generate($this->record->content, storage_path('app/qr-codes/' . $this->record->id . '.svg'));
+                    return response()->download(storage_path('app/qr-codes/' . $this->record->id . '.svg'));
+
+                })
                 ->icon('heroicon-o-document-download')
                 ->modalHeading('Preview QR-code')
                 ->modalButton('Download')
